cdb4a5cfc06841cb7f052c475e455a3042d264c9
Nacho <n.etche@yandex.com>
2022-04-29 13:02:06 +0200

    fix module not found

--- auth_service/src/auth.js
+++ auth_service/src/auth.js
@@ -17,7 +17,7 @@ const users = [
   },
 ];
 
-class AuthError extends Error {}
+class AuthError extends Error { }
 
 const authFactory = (secret) => (username, password) => {
   const user = users.find((u) => u.username === username);
